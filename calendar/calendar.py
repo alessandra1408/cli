3cbf355ab06006af9adae22f3c3dd7f89daf5edd
alessandra1408 <alessandraoitnb@gmail.com>
2022-09-24 21:25:53 -0300

    Create cli open google calendar

---
+++ calendar/calendar.py
@@ -0,0 +1,9 @@
+import pyautogui
+from time import sleep
+
+#CLI to open google calendar in chrome
+
+pyautogui.click(588,40, duration=2)
+pyautogui.click(367,81, duration=1)
+pyautogui.write('Calendar')
+pyautogui.click(382,114, duration=1)
